8027a765caca55bf7ee7b38260d73cc5ab2bfc7b
AlexanderRest <restzone0964@gmail.com>
2025-03-05 22:33:31 +0300

    Add files via upload

---
+++ Prakk2/build/CMakeFiles/my_utility.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sa/Prakk2/error_handler.c" "/home/sa/Prakk2/build/CMakeFiles/my_utility.dir/error_handler.c.o"
+  "/home/sa/Prakk2/help.c" "/home/sa/Prakk2/build/CMakeFiles/my_utility.dir/help.c.o"
+  "/home/sa/Prakk2/logger.c" "/home/sa/Prakk2/build/CMakeFiles/my_utility.dir/logger.c.o"
+  "/home/sa/Prakk2/main.c" "/home/sa/Prakk2/build/CMakeFiles/my_utility.dir/main.c.o"
+  "/home/sa/Prakk2/processes.c" "/home/sa/Prakk2/build/CMakeFiles/my_utility.dir/processes.c.o"
+  "/home/sa/Prakk2/users.c" "/home/sa/Prakk2/build/CMakeFiles/my_utility.dir/users.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Prakk2/build/CMakeFiles/my_utility.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_utility.dir/error_handler.c.o"
+  "CMakeFiles/my_utility.dir/help.c.o"
+  "CMakeFiles/my_utility.dir/logger.c.o"
+  "CMakeFiles/my_utility.dir/main.c.o"
+  "CMakeFiles/my_utility.dir/processes.c.o"
+  "CMakeFiles/my_utility.dir/users.c.o"
+  "my_utility"
+  "my_utility.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_utility.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
